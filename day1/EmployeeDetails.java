1c77a219c3471be43f5c7db2fef807b15f7bd400
spreethij18 <spreethij18@gmail.com>
2025-10-12 13:50:51 +0530

    Add files via upload
    Day 1 Practice

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Created different variables and assigned values for Employee Details
+		String name = "Preethi S";
+		int empid = 1001;
+		int age = 35;
+		char gender = 'F';
+		float salary = 50000.75812918f;
+		String city = "Bengaluru";
+		boolean currentemp = true;
+		
+		//Printing the Employee Details
+		System.out.println("EMPLOYEE DETAILS:");
+		System.out.println("_________________");
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Gender: "+gender);
+		System.out.println("Salary: "+salary);
+		System.out.println("City: "+city);
+		System.out.println("Present in Org currently: "+currentemp);
+		System.out.println("All Details are Printed. Thank You.");
+		
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+import java.security.DomainCombiner;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Test Leaf");
+		System.out.print("Welcome");
+	}	
+
+
+}
