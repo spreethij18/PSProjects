8896ae8b2f3ffb5649ec74701d74a76265a91029
spreethij18 <spreethij18@gmail.com>
2025-10-17 15:01:09 +0530

    Add files via upload

--- day2/IsPrime.java
+++ day2/IsPrime.java
@@ -5,19 +5,39 @@ public class IsPrime {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		int n=10;
-		
-		for (int i=2;i<=n-1;i++)
+		int num=133; //number to be checked if prime or not
+		int factor=0; //Prime number has 2 factors. Initializing the factor to 0.
+		System.out.println("Number you are checking is:"+num ); //Printing the number that is being checked for prime or not
+		for (int i=1;i<=num;i++)
 		{
-			System.out.println("Iteration #"+i);
-			if (n%i==0) 
+			if (num%i==0) 
 			{
-				System.out.println(n+"is Prime"); 
+				factor++; 
 			}
-			/*else
-			{
-				System.out.println(i+"is Prime");
-			}*/
+		}
+		
+		//Output details if the number to be checked is less than or equals 0.
+		if(num<=0)
+		{
+			System.out.println("The number you check is invalid");
+		}
+		
+		//Output details if the number to be checked is 1.
+		if(num==1)
+		{
+			System.out.println("It is NOT a Prime number");
+		}
+		
+		//Output details if the number to be checked is having exactly 2 factors i.e 1 and the number itself. So factor count=2.
+		if(factor==2)
+		{
+			System.out.println("It is a Prime number");
+		}
+		
+		//Output details if the number to be checked is having more than 2 factors.
+		if(factor>2)
+		{
+			System.out.println("It is not a Prime number");
 		}
 
 	}
